bbc2d616b65676ba30f32f1bb765b07634eb862b
r1oga <38692952+r1oga@users.noreply.github.com>
2022-11-04 17:21:04 +0100

    Extract Twitter routes in own controller

--- src/services/http/controllers/index.ts
+++ src/services/http/controllers/index.ts
@@ -2,5 +2,6 @@ export * from './Events';
 export * from './Interep';
 export * from './Posts';
 export * from './Tags';
+export * from './Twitter';
 export * from './Users';
 export * from './ZkChat';

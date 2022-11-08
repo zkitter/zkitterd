a9aefc23ad2b484c08a841c0f16b49cf0d7adcd6
r1oga <38692952+r1oga@users.noreply.github.com>
2022-11-08 21:59:10 +0100

    get receivedStars/proPlan when authenticating

--- src/services/http/controllers/Github.ts
+++ src/services/http/controllers/Github.ts
@@ -4,33 +4,54 @@ import { Strategy as GhStrategy } from 'passport-github2';
 
 import { Controller } from './interface';
 import config from '../../../util/config';
+import { getReceivedStars } from '../../../util/github';
+
+const options = {
+  clientID: config.ghClientId,
+  clientSecret: config.ghClientSecret,
+  callbackURL: config.ghCallbackUrl,
+};
 
 export class GithubController extends Controller {
   constructor() {
     super();
     passport.use(
       new GhStrategy(
-        {
-          clientID: config.ghClientId,
-          clientSecret: config.ghClientSecret,
-          callbackURL: config.ghCallbackUrl,
-        },
+        options,
         // @ts-ignore
         async (accessToken, refreshToken, profile, done) => {
           const {
             id: userId,
             username,
             displayName,
-            _json: { followers },
+            _json: {
+              followers,
+              plan: { name },
+            },
           } = profile;
+
+          const proPlan = name === 'pro';
+          const receivedStars = await getReceivedStars(username);
+
           const githubAuthDB = await this.call('db', 'getGithubAuth');
+
           await githubAuthDB.upsertOne({
             userId,
             username,
             displayName,
             followers,
+            proPlan,
+            receivedStars,
           });
-          return done(null, { userId, username, displayName, followers });
+
+          return done(null, {
+            userId,
+            username,
+            displayName,
+            followers,
+            proPlan,
+            receivedStars,
+          });
         }
       )
     );
@@ -44,9 +65,9 @@ export class GithubController extends Controller {
       Router()
         .get('', passport.authenticate('github', { scope: ['read:user', 'read:org'] }))
         .get('/callback', passport.authenticate('github', { failWithError: true }), this.callback)
-      // .get('/test', (req, res) => {
-      //   res.json(req.user);
-      // })
+        .get('/test', (req, res) => {
+          res.json(req.user);
+        })
     );
   };
 

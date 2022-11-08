b651b14e6f79b063179d07e748b34ff02d7a6d00
r1oga <38692952+r1oga@users.noreply.github.com>
2022-11-09 00:04:17 +0100

    remove console log

--- src/services/http/controllers/static.ts
+++ src/services/http/controllers/static.ts
@@ -5,7 +5,6 @@ const path = (...args: string[]) => _path.join(process.cwd(), 'static', ...args)
 
 const router = Router();
 
-console.log(path('semaphore_final.zkey'));
 router.use('/dev/semaphore_wasm', express.static(path('semaphore.wasm')));
 router.use('/dev/semaphore_final_zkey', express.static(path('semaphore_final.zkey')));
 router.use('/dev/semaphore_vkey', express.static(path('verification_key.json')));

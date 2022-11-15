a2dea4c905d027676b02109098bdae13f0794a62
r1oga <38692952+r1oga@users.noreply.github.com>
2022-11-16 00:03:23 +0100

    fix redirect

--- src/services/http/controllers/Github.ts
+++ src/services/http/controllers/Github.ts
@@ -1,11 +1,11 @@
-import { Request, Response, Router } from 'express';
+import {Request, RequestHandler, Response, Router} from 'express';
 import passport from 'passport';
-import { Strategy as GhStrategy } from 'passport-github2';
-import { calculateReputation, OAuthProvider } from '@interep/reputation';
+import {Strategy as GhStrategy} from 'passport-github2';
+import {calculateReputation, OAuthProvider} from '@interep/reputation';
 
-import { Controller } from './interface';
+import {Controller} from './interface';
 import config from '../../../util/config';
-import { getReceivedStars } from '../../../util/github';
+import {getReceivedStars} from '../../../util/github';
 
 type GhUser = {
   id: number;
@@ -17,59 +17,53 @@ type GhUser = {
 };
 
 export class GithubController extends Controller {
+  redirectUrl: string;
+
   constructor() {
     super();
     passport.use(
-      new GhStrategy(
-        {
-          clientID: config.ghClientId,
-          clientSecret: config.ghClientSecret,
-          callbackURL: config.ghCallbackUrl,
-          passReqToCallback: true,
-        },
-        async (
-          req: Request,
-          accessToken: string,
-          refreshToken: string,
-          profile: GhUser,
-          done: any
-        ) => {
-          const {
-            id: userId,
-            username,
-            _json: {
-              followers,
-              plan: { name: planName },
+        new GhStrategy(
+            {
+              clientID: config.ghClientId,
+              clientSecret: config.ghClientSecret,
+              callbackURL: config.ghCallbackUrl,
             },
-          } = profile;
+            async (accessToken: string, refreshToken: string, profile: GhUser, done: any) => {
+              const {
+                id: userId,
+                username,
+                _json: {
+                  followers,
+                  plan: {name: planName},
+                },
+              } = profile;
 
-          const proPlan = planName === 'pro';
-          const receivedStars = await getReceivedStars(username);
-          const reputation = calculateReputation(OAuthProvider.GITHUB, {
-            followers,
-            receivedStars,
-            proPlan,
-          });
+              const proPlan = planName === 'pro';
+              const receivedStars = await getReceivedStars(username);
+              const reputation = calculateReputation(OAuthProvider.GITHUB, {
+                followers,
+                receivedStars,
+                proPlan,
+              });
 
-          // TODO: need for github_auths DB operation here?
-          // const githubAuthDB = await this.call('db', 'getGithubAuth');
-          //
-          // await githubAuthDB.upsertOne({
-          //   userId,
-          //   username,
-          //   displayName,
-          //   followers,
-          //   proPlan,
-          //   receivedStars,
-          // });
+              // TODO: need for github_auths DB operation here?
+              // const githubAuthDB = await this.call('db', 'getGithubAuth');
+              //
+              // await githubAuthDB.upsertOne({
+              //   userId,
+              //   username,
+              //   displayName,
+              //   followers,
+              //   proPlan,
+              //   receivedStars,
+              // });
 
-          req.redirectUrl = req.params.redirectUrl;
-          return done(null, {
-            userId,
-            username,
-            reputation,
-          });
-        }
+              return done(null, {
+                userId,
+                username,
+                reputation,
+              });
+            }
       )
     );
 
@@ -78,17 +72,23 @@ export class GithubController extends Controller {
 
   addRoutes = () => {
     this._router.use(
-      '/auth/github',
-      Router()
-        .get('', passport.authenticate('github', { scope: ['read:user', 'read:org'] }))
-        .get('/callback', passport.authenticate('github', { failWithError: true }), this.callback)
-      // .get('/test', (req, res) => {
-      //   res.json(req.user);
-      // })
+        '/auth/github',
+        Router()
+            .get(
+                '',
+                this.storeRedirectUrl,
+                passport.authenticate('github', {scope: ['read:user', 'read:org']})
+            )
+            .get('/callback', passport.authenticate('github', {failWithError: true}), this.callback)
     );
   };
 
   callback = (req: Request, res: Response) => {
-    res.redirect(req.redirectUrl);
+    res.redirect(this.redirectUrl);
+  };
+
+  storeRedirectUrl: RequestHandler<{}, {}, {}, { redirectUrl: string }> = (req, res, next) => {
+    this.redirectUrl = req.query.redirectUrl;
+    next();
   };
 }

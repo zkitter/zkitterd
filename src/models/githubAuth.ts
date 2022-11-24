02e965300b8cc79fdb61bdd4a8b75b95f677c511
r1oga <38692952+r1oga@users.noreply.github.com>
2022-11-24 15:57:55 +0100

    upsert GH accessToken in DB when authenticating

--- src/models/githubAuth.ts
+++ src/models/githubAuth.ts
@@ -1,12 +1,8 @@
-import { BOOLEAN, INTEGER, Sequelize, STRING } from 'sequelize';
+import { Sequelize, STRING } from 'sequelize';
 
 export type GithubAuthModel = {
   userId: string;
-  username: string;
-  displayName: string | null;
-  followers: number;
-  receivedStars: number;
-  proPlan: boolean;
+  accessToken: string;
 };
 
 const githubAuth = (sequelize: Sequelize) => {
@@ -14,31 +10,22 @@ const githubAuth = (sequelize: Sequelize) => {
     'github_auth',
     {
       userId: { type: STRING },
-      username: { type: STRING },
-      displayName: { type: STRING },
-      followers: { type: INTEGER },
-      receivedStars: { type: INTEGER },
-      proPlan: { type: BOOLEAN },
+      accessToken: { type: STRING },
     },
     {
-      indexes: [
-        { unique: true, fields: ['userId'] },
-        { unique: true, fields: ['username'] },
-        { unique: true, fields: ['displayName'] },
-      ],
+      indexes: [{ unique: true, fields: ['userId'] }],
     }
   );
 
   const findUserById = async (userId: string) =>
-    (await model.findOne({ where: { userId } }))?.toJSON() as GithubAuthModel;
-  const findUserByUserName = async (username: string) =>
-    (await model.findOne({ where: { username } }))?.toJSON() as GithubAuthModel;
+    (
+      await model.findOne({ where: { userId }, attributes: ['accessToken'] })
+    )?.toJSON() as GithubAuthModel;
   const upsertOne = async (data: GithubAuthModel) => (await model.upsert(data))[0];
 
   return {
     model,
     findUserById,
-    findUserByUserName,
     upsertOne,
   };
 };

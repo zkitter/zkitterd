51c2d2e51ab1de08b225a27c09b6a2a457ff38ea
tsukino <87639218+0xtsukino@users.noreply.github.com>
2022-12-08 15:47:09 +0800

    update search query to allow multiple search params (#56)
    * update search query to allow multiple search params
    
    * fix and test search query's replacement regex (#57)
    
    * fix and test search query's replacement regex
    
    * fix interep http test
    
    Co-authored-by: r1oga <38692952+r1oga@users.noreply.github.com>

--- src/models/posts.ts
+++ src/models/posts.ts
@@ -508,11 +508,18 @@ const posts = (sequelize: Sequelize) => {
       // prettier-ignore
       .query(
         `SELECT *
-              FROM posts
-              WHERE ts @@ to_tsquery('english', :query)
-              ORDER BY "createdAt" ${order} LIMIT :limit
-              OFFSET :offset`,
-        { type: QueryTypes.SELECT, replacements: { limit, offset, query } }
+         FROM posts
+         WHERE ts @@ to_tsquery('english', :query)
+         ORDER BY "createdAt" ${order} LIMIT :limit
+         OFFSET :offset`,
+        {
+          type: QueryTypes.SELECT,
+          replacements: {
+            limit,
+            offset,
+            query: query.replace(/\||,/g, ' ').replace(/\s\s+/g, ' ').replace(/ /g, ' | '),
+          },
+        }
       )
       .then(result => result.map(inflateResultToPostJSON));
 
